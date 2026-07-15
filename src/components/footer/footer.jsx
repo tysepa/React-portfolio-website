da6b2762cf0a109d61eccaad933ed8376285f4e9
tysepa <tuyiepa88@gmail.com>
2026-07-15 20:53:59 +0200

    Fix all remaining ESLint warnings to enable CI build

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -8,9 +8,9 @@ import {IoLogoTwitter } from 'react-icons/io'
 const footer = () => {
   return (
     <footer >
-      <a href="#" className='footer__logo'>Epaphrodis</a>
+      <a href="#home" className='footer__logo'>Epaphrodis</a>
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
